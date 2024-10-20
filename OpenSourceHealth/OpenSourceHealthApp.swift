d594980af8fa618ac76c2d9df3d284fed186cbe4
Kanishk <kanishkjain01@icloud.com>
2024-10-20 12:26:59 -0400

    Initial Commit

---
+++ OpenSourceHealth/OpenSourceHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSourceHealthApp.swift
+//  OpenSourceHealth
+//
+//  Created by Kanishk Jain on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSourceHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
